5c3f3c0316bf57b71c30ea9fb340c7b50a0b1898
Shalaka <sharmask@iu.edu>
2017-04-28 14:23:10 -0400

    preferences

--- src/main/java/com/actionlistapi/model/Preferences.java
+++ src/main/java/com/actionlistapi/model/Preferences.java
@@ -2,28 +2,46 @@ package com.actionlistapi.model;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
-import javax.persistence.SequenceGenerator;
+
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 public class Preferences {
 	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY,generator="pref_id_seq")
-	@SequenceGenerator(name="pref_id_seq",sequenceName="pref_id_seq",allocationSize=1)
-	private long id;
+	@Id 
+	@GeneratedValue(generator="uuid")
+	@GenericGenerator(name="uuid",strategy = "uuid")
+	private String id;
 	
 	@OneToOne
 	private Colors colors;
 
+	private String username;
+	
 	public Colors getColors() {
 		return colors;
 	}
 
 	public void setColors(Colors colors) {
 		this.colors = colors;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
 	}
 	
 }
--- src/main/java/com/actionlistapi/repository/PreferencesInterface.java
+++ src/main/java/com/actionlistapi/repository/PreferencesInterface.java
@@ -4,8 +4,12 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.actionlistapi.model.Preferences;
+import java.lang.String;
+import java.util.List;
 
 @RepositoryRestResource(collectionResourceRel = "preferences", path = "preferences")
 public interface PreferencesInterface extends PagingAndSortingRepository<Preferences, String>{
 	
+	List<Preferences> findByUsername(String username);
+
 }
